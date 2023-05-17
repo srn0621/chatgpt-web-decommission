1d8a94a933037b0828bff77b1f8673ee9d24540f
srn0621 <ruonanshang@gmail.com>
2023-05-17 08:48:37 +0800

    Update GithubSite.vue

--- src/components/custom/GithubSite.vue
+++ src/components/custom/GithubSite.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="text-neutral-400">
     <span>Star on</span>
-    <a href="https://github.com/Chanzhaoyu/chatgpt-bot" target="_blank" class="text-blue-500">
+    <a href="" target="_blank" class="text-blue-500">
       GitHub
     </a>
   </div>
